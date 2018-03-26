7f665f4e7e55e299a3755074a5ea54114cee71ef
Eric Youngson <eayoungs@gmail.com>
2018-03-25 20:50:53 -0700

    Reverted back to html based response

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import socket
 pins = [machine.Pin(i, machine.Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
 adc = machine.ADC(0)
 
+html = """<!DOCTYPE html>
+<html>
+    <head> <title>ESP8266 Pins</title> </head>
+    <body> <h1>ESP8266 Pins</h1>
+        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+    </body>
+</html>
+"""
+
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
 s = socket.socket()
@@ -21,10 +30,8 @@ while True:
         line = cl_file.readline()
         if not line or line == b'\r\n':
             break
-
-    rows = [p.value() for p in pins]
-    rows.append(adc.read())
-
-    response = rows
+    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+    rows.append('<tr><td>%s</td><td>%d</td></tr>' % ('ADC(0)', adc.read()))
+    response = html % '\n'.join(rows)
     cl.send(response)
     cl.close()
