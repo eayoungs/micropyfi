78bb654bc56ea4009778c6f4d8baafb25f5a9167
Eric Youngson <eayoungs@gmail.com>
2018-03-27 20:45:55 -0700

    Added fixed ip on boot

--- boot.py
+++ boot.py
@@ -1,6 +1,6 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
+# import esp
+# esp.osdebug(None)
 import gc
 import webrepl
 
@@ -10,6 +10,8 @@ gc.collect()
 def do_connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
+    sta_if.ifconfig(('192.168.0.111', '255.255.255.0', '192.168.0.1' ,
+                     '8.8.8.8'))
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
