d76a7f7cb2b8305d1ddeeabf7a79c5488e59475f
Eric Youngson <eayoungs@gmail.com>
2018-04-22 18:35:20 -0700

    Removed unused html

--- main.py
+++ main.py
@@ -14,34 +14,18 @@ s.listen(1)
 
 print('listening on', addr)
 
-f_name = "adc.log"
-
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-        return i + 1
-
+while True:
+    cl, addr = s.accept()
+    print('client connected from', addr)
+    cl_file = cl.makefile('rwb', 0)
     while True:
-        cl, addr = s.accept()
-        print('client connected from', addr)
-        cl_file = cl.makefile('rwb', 0)
-        while True:
-            line = cl_file.readline()
-            if not line or line == b'\r\n':
-                break
+        line = cl_file.readline()
+        if not line or line == b'\r\n':
+            break
 
-        response_dict = dict([(str(p), p.value()) for p in pins])
-        response_dict['ADC(0)'] = adc.read()
-        json_response = ujson.dumps(response_dict)
+    response_dict = dict([(str(p), p.value()) for p in pins])
+    response_dict['ADC(0)'] = adc.read()
+    json_response = ujson.dumps(response_dict)
 
-        if file_len(f_name) < 2:
-            f_mode = 'a'
-        else:
-            f_mode = 'w'
-        with open(f_name, mode=f_mode, encoding='utf-8') as f:
-            f.write(json_response)
-            f.write('\n')
-
-        cl.send(json_response)
-        cl.close()
+    cl.send(json_response)
+    cl.close()
