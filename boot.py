ea42f4c73878d91e9720ef9a58515731d2202bb1
Eric Youngson <eayoungs@gmail.com>
2018-06-22 13:32:00 -0700

    Added files to support websockets in main.py, removed old code from boot.py

--- boot.py
+++ boot.py
@@ -10,8 +10,6 @@ gc.collect()
 def do_connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
-    # network.WLAN.ifconfig(config=('fixed_ip', '255.255.255.0',
-    #                               'router_ip', 'routers_ip'))
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
@@ -19,6 +17,8 @@ def do_connect():
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.config(True)
 
 do_connect()
 webrepl.start()
